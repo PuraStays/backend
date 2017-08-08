f9f2d6e0bbcbc1b1ca6c4b60302f035bfd6225e9
ranjan <kumar.ranjan89@gmail.com>
2017-08-08 23:10:36 +0530

    commentout express status monitor

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressStatusMonitor());
+//app.use(expressStatusMonitor());
 
 /**
  * Website routes
